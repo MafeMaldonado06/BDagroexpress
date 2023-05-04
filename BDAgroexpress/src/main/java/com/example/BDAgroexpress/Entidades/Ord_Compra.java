f7953a9628e4b71f45d4908a57b321c99d7ccf6b
Julain S <Salazardix321@gmail.com>
2023-05-03 22:23:36 -0500

    Julianv1

---
+++ BDAgroexpress/src/main/java/com/example/BDAgroexpress/Entidades/Ord_Compra.java
@@ -0,0 +1,112 @@
+package com.example.BDAgroexpress.Entidades;
+import jakarta.persistence.*;
+
+import java.util.Set;
+@Entity
+@Table(name = "Ordencomra")
+public class Ord_Compra {
+
+    @Id
+    @Column(unique = true, length = 20)
+    private String OrdC_Id;
+
+    @Column(nullable = false)
+    private int OrdC_IdComp;
+
+    @Column(unique = true, length = 20)
+    private String OrdC_IdVenta;
+
+    @Column(nullable = false)
+    private int OrdC_IdProducto;
+
+    @Column(unique = true, length = 20)
+    private String OrdC_Fecha;
+
+    @Column(nullable = false)
+    private int OrdC_Totalpagar;
+
+    @Column(nullable = false)
+    private int OrdC_Cantcoprada;
+
+    public Ord_Compra(String ordC_Id, int ordC_IdComp, String ordC_IdVenta, int ordC_IdProducto, String ordC_Fecha, int ordC_Totalpagar, int ordC_Cantcoprada) {
+        OrdC_Id = ordC_Id;
+        OrdC_IdComp = ordC_IdComp;
+        OrdC_IdVenta = ordC_IdVenta;
+        OrdC_IdProducto = ordC_IdProducto;
+        OrdC_Fecha = ordC_Fecha;
+        OrdC_Totalpagar = ordC_Totalpagar;
+        OrdC_Cantcoprada = ordC_Cantcoprada;
+    }
+
+    public Ord_Compra() {
+    }
+
+    public String getOrdC_Id() {
+        return OrdC_Id;
+    }
+
+    public void setOrdC_Id(String ordC_Id) {
+        OrdC_Id = ordC_Id;
+    }
+
+    public int getOrdC_IdComp() {
+        return OrdC_IdComp;
+    }
+
+    public void setOrdC_IdComp(int ordC_IdComp) {
+        OrdC_IdComp = ordC_IdComp;
+    }
+
+    public String getOrdC_IdVenta() {
+        return OrdC_IdVenta;
+    }
+
+    public void setOrdC_IdVenta(String ordC_IdVenta) {
+        OrdC_IdVenta = ordC_IdVenta;
+    }
+
+    public int getOrdC_IdProducto() {
+        return OrdC_IdProducto;
+    }
+
+    public void setOrdC_IdProducto(int ordC_IdProducto) {
+        OrdC_IdProducto = ordC_IdProducto;
+    }
+
+    public String getOrdC_Fecha() {
+        return OrdC_Fecha;
+    }
+
+    public void setOrdC_Fecha(String ordC_Fecha) {
+        OrdC_Fecha = ordC_Fecha;
+    }
+
+    public int getOrdC_Totalpagar() {
+        return OrdC_Totalpagar;
+    }
+
+    public void setOrdC_Totalpagar(int ordC_Totalpagar) {
+        OrdC_Totalpagar = ordC_Totalpagar;
+    }
+
+    public int getOrdC_Cantcoprada() {
+        return OrdC_Cantcoprada;
+    }
+
+    public void setOrdC_Cantcoprada(int ordC_Cantcoprada) {
+        OrdC_Cantcoprada = ordC_Cantcoprada;
+    }
+
+    @Override
+    public String toString() {
+        return "Ord_Compra{" +
+                "OrdC_Id='" + OrdC_Id + '\'' +
+                ", OrdC_IdComp=" + OrdC_IdComp +
+                ", OrdC_IdVenta='" + OrdC_IdVenta + '\'' +
+                ", OrdC_IdProducto=" + OrdC_IdProducto +
+                ", OrdC_Fecha='" + OrdC_Fecha + '\'' +
+                ", OrdC_Totalpagar=" + OrdC_Totalpagar +
+                ", OrdC_Cantcoprada=" + OrdC_Cantcoprada +
+                '}';
+    }
+}
